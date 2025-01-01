0710cf14985f225ff7eadb1e869b7faf528b46bd
omaroskars <omar@konstant.is>
2025-01-01 08:01:18 +0100

    fixup

--- src/payload/plugin.ts
+++ src/payload/plugin.ts
@@ -1,6 +1,6 @@
 import { Config } from "payload";
 
-export const payloadFieldsPlugin = (): ((config: Config) => Config) => {
+export const konstantFieldsPlugin = (): ((config: Config) => Config) => {
   return (config) => {
     // You can extend the Payload config here if needed
     console.log("Payload Fields Plugin Loaded");
