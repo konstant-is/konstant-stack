8ffe863122d1acdcb23b4e4051687228fca1e769
omaroskars <omar@konstant.is>
2025-01-01 11:22:31 +0100

    fixup

--- src/payload/plugin.ts
+++ src/payload/plugin.ts
@@ -1,10 +1,7 @@
 import { Config, Plugin } from "payload";
 
-export const konstantFieldsPlugin: Plugin = async (
-  config: Config,
-): Promise<Config> => {
-  // You can extend the Payload config here if needed
-
-  // Modify the incomingConfig as needed
-  return config;
-};
+export const konstantFieldsPlugin =
+  (pluginOptions?: {}) =>
+  (config: Config): Config => {
+    return config;
+  };
