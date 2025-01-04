15220e3d05b0a6865a32366372c3b4f6b208047d
omaroskars <omar@konstant.is>
2025-01-04 10:28:30 +0100

    chore: update dist [auto-generated]

--- dist/payload/index.d.ts
+++ dist/payload/index.d.ts
@@ -24,7 +24,7 @@ type Props = {
         checkboxOverrides?: Partial<CheckboxField>;
     };
 };
-declare const slugField: (props: Props) => [Field, Field];
+declare const slugField: (props?: Props) => [Field, Field];
 
 declare const uriField: () => payload.Field;
 
