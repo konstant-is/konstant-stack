804d2ae96d254561c5fa64b2f4aafa4cab9eabb1
omaroskars <omar@konstant.is>
2025-01-25 09:38:17 +0100

    return T

--- src/payload/utils/getRelation.ts
+++ src/payload/utils/getRelation.ts
@@ -1,37 +1,37 @@
-import { getReference } from './getReference.js'
+import { getReference } from "./getReference.js";
 
 type RelationProps<T> = {
-  relationTo: string
-  value: string | T
-}
+  relationTo: string;
+  value: string | T;
+};
 
-type FetchQuery<T> = (params: { collection: string; id: string }) => Promise<T>
+type FetchQuery<T> = (params: { collection: string; id: string }) => Promise<T>;
 
 export const getRelation = <T>(props: RelationProps<T>) => {
-  const { relationTo, value } = props
+  const { relationTo, value } = props;
 
-  const getValue = (): null | T => getReference(value)
+  const getValue = (): null | T => getReference(value);
 
   const fetch = async (query: FetchQuery<T>) => {
     return await query({
       id: value as string,
       collection: relationTo,
-    })
-  }
+    });
+  };
 
-  const getOrFetchValue = async (query: FetchQuery<T>): Promise<null | T> => {
-    const resolvedValue = getValue()
+  const getOrFetchValue = async (query: FetchQuery<T>): Promise<T> => {
+    const resolvedValue = getValue();
     if (resolvedValue !== null) {
-      return resolvedValue
+      return resolvedValue;
     }
 
-    const result = fetch(query)
-    return result
-  }
+    const result = fetch(query);
+    return result;
+  };
 
   return {
     getOrFetchValue,
     getValue,
     relationTo,
-  }
-}
+  };
+};
